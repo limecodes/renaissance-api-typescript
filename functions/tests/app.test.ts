737fc1820fb6b0e6d5501e785bae25af5505c620
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-09-24 16:30:42 +0300

    Setup and running tests & setup folder structure for api routes.

--- functions/tests/app.test.ts
+++ functions/tests/app.test.ts
@@ -1,3 +1,30 @@
-test('Placeholder test', () => {
-  expect(true).toBeTruthy();
+import * as http from 'http';
+import * as request from 'supertest';
+import express from 'express';
+
+// Use separate env file for tests
+import config from '../src/config';
+
+let server: http.Server, agent: request.SuperAgentTest;
+
+// Later, refactor all this into setupTests file
+beforeEach(async done => {
+  const app = express();
+
+  await require('../src/loaders').default({ expressApp: app });
+
+  server = app.listen(config.port).on('error', err => {
+    console.log('Error creating server', err);
+    done(err);
+  });
+  agent = request.agent(server);
+  done();
+});
+
+afterEach(done => {
+  return server && server.close(done);
+});
+
+test('Health check should be OK', () => {
+  return agent.get('/status').expect(200);
 });
