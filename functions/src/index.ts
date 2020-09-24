ca31e306cdf307b1560fb620af9b560fe6c2f6ec
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-09-24 18:38:54 +0300

    Refining build process for CF functions and adding a workflow on push.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,4 @@
-/* For now while I'm still setting up */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import app from './app';
+/* eslint-disable import/prefer-default-export */
+export const api = functions.https.onRequest(app);
